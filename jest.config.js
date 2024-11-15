c9e1ddf2a26775faa86cbeac4210a04b5c8eb1ce
Ludvig Mathias Sti <69594206+LudvigSti@users.noreply.github.com>
2024-11-15 13:36:19 +0100

    fix tests? part 2

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testEnvironment: 'jsdom',
-  };
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@testing-library)',
+  ],
+};
