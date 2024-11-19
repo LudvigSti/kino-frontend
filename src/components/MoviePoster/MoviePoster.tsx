2475ae94bff0aa57c2d8b7c1423ebbc7c193e3d9
IbbiSecka <s355423@oslomet.no>
2024-11-19 13:30:10 +0100

    Fetched movies successfully. pressing them takes you to its MoviePage. Movie page with a secondary image together with its movie trailer right under, youtube trailer doesnt work yet. Fixed padding to the Appheader. Still hardcoded screenings

--- src/components/MoviePoster/MoviePoster.tsx
+++ src/components/MoviePoster/MoviePoster.tsx
@@ -3,12 +3,14 @@ import './moviePoster.css';
 
 interface MoviePosterProps {
   image: string;
+  title: string;
 }
 
-const MoviePoster: React.FC<MoviePosterProps> = ({ image }) => {
+const MoviePoster: React.FC<MoviePosterProps> = ({ image, title }) => {
   return (
     <div className='movie-poster'>
-      <img src={image} alt='Movie Poster' />
+      <img src={image} alt={`${title}`} />
+      <p className='movite-title'>{title}</p>
     </div>
   );
 };
