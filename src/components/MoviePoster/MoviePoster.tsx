b628a479eacac3a6f58eef383e13cd52c570e332
IbbiSecka <s355423@oslomet.no>
2024-11-21 10:38:47 +0100

    Added details to landing page(Font on section text, star and rating on highest rated movies) and loading page when navigating to a movie page

--- src/components/MoviePoster/MoviePoster.tsx
+++ src/components/MoviePoster/MoviePoster.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './moviePoster.css';
+import { FaStar } from 'react-icons/fa'; // Import a star icon from react-icons
+
 
 interface MoviePosterProps {
   image: string;
   title: string;
+  rating?: number;
 }
 
-const MoviePoster: React.FC<MoviePosterProps> = ({ image, title }) => {
+const MoviePoster: React.FC<MoviePosterProps> = ({ image, title, rating }) => {
   return (
     <div className='movie-poster'>
       <img src={image} alt={`${title}`} />
-      <p className='movite-title'>{title}</p>
+      <p className='movie-title'>{title}</p>
+      {rating !== undefined && (
+        <p className='movie-rating'>
+          <FaStar className='star-icon' /> <p className='rating-text'>{rating}</p>
+        </p>
+        
+      )}
     </div>
   );
 };
+
 
 export default MoviePoster;
